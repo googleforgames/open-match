f0554a341f56aa8251ef0c8000856b8a8a9eb7a9
Jeremy Edwards <jeremyje@gmail.com>
2019-02-20 10:02:06 -0800

    Move application logic from cmd/ to internal/app/

--- cmd/frontendapi/main.go
+++ cmd/frontendapi/main.go
@@ -19,87 +19,17 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package main
 
 import (
-	"errors"
-	"os"
-	"os/signal"
-
-	"github.com/GoogleCloudPlatform/open-match/cmd/frontendapi/apisrv"
-	"github.com/GoogleCloudPlatform/open-match/config"
-	"github.com/GoogleCloudPlatform/open-match/internal/logging"
-	"github.com/GoogleCloudPlatform/open-match/internal/metrics"
-	redishelpers "github.com/GoogleCloudPlatform/open-match/internal/statestorage/redis"
-
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
-	"go.opencensus.io/plugin/ocgrpc"
-)
-
-var (
-	// Logrus structured logging setup
-	feLogFields = log.Fields{
-		"app":       "openmatch",
-		"component": "frontend",
-	}
-	feLog = log.WithFields(feLogFields)
-
-	// Viper config management setup
-	cfg = viper.New()
-	err = errors.New("")
+	"github.com/GoogleCloudPlatform/open-match/internal/app/frontendapi"
 )
 
 func init() {
-	// Add a hook to the logger to auto-count log lines for metrics output thru OpenCensus
-	log.AddHook(metrics.NewHook(apisrv.FeLogLines, apisrv.KeySeverity))
-
-	// Add a hook to the logger to log the filename & line number.
-	log.SetReportCaller(true)
-
-	// Viper config management initialization
-	cfg, err = config.Read()
-	if err != nil {
-		feLog.WithFields(log.Fields{
-			"error": err.Error(),
-		}).Error("Unable to load config file")
-	}
-
-	// Configure open match logging defaults
-	logging.ConfigureLogging(cfg)
-
-	// Configure OpenCensus exporter to Prometheus
-	// metrics.ConfigureOpenCensusPrometheusExporter expects that every OpenCensus view you
-	// want to register is in an array, so append any views you want from other
-	// packages to a single array here.
-	ocServerViews := apisrv.DefaultFrontendAPIViews                     // FrontendAPI OpenCensus views.
-	ocServerViews = append(ocServerViews, ocgrpc.DefaultServerViews...) // gRPC OpenCensus views.
-	ocServerViews = append(ocServerViews, config.CfgVarCountView)       // config loader view.
-	// Waiting on https://github.com/opencensus-integrations/redigo/pull/1
-	// ocServerViews = append(ocServerViews, redis.ObservabilityMetricViews...) // redis OpenCensus views.
-	feLog.WithFields(log.Fields{"viewscount": len(ocServerViews)}).Info("Loaded OpenCensus views")
-	metrics.ConfigureOpenCensusPrometheusExporter(cfg, ocServerViews)
+	frontendapi.InitializeApplication()
 }
 
 func main() {
-
-	// Connect to redis
-	pool := redishelpers.ConnectionPool(cfg)
-	defer pool.Close()
-
-	// Instantiate the gRPC server with the connections we've made
-	feLog.Info("Attempting to start gRPC server")
-	srv := apisrv.New(cfg, pool)
-
-	// Run the gRPC server
-	err := srv.Open()
-	if err != nil {
-		feLog.WithFields(log.Fields{"error": err.Error()}).Fatal("Failed to start gRPC server")
-	}
-
-	// Exit when we see a signal
-	terminate := make(chan os.Signal, 1)
-	signal.Notify(terminate, os.Interrupt)
-	<-terminate
-	feLog.Info("Shutting down gRPC server")
+	frontendapi.RunApplication()
 }
--- cmd/mmlogicapi/main.go
+++ cmd/mmlogicapi/main.go
@@ -22,83 +22,13 @@ limitations under the License.
 package main
 
 import (
-	"errors"
-	"os"
-	"os/signal"
-
-	"github.com/GoogleCloudPlatform/open-match/cmd/mmlogicapi/apisrv"
-	"github.com/GoogleCloudPlatform/open-match/config"
-	"github.com/GoogleCloudPlatform/open-match/internal/metrics"
-	redisHelpers "github.com/GoogleCloudPlatform/open-match/internal/statestorage/redis"
-
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
-	"go.opencensus.io/plugin/ocgrpc"
-)
-
-var (
-	// Logrus structured logging setup
-	mlLogFields = log.Fields{
-		"app":       "openmatch",
-		"component": "mmlogic",
-	}
-	mlLog = log.WithFields(mlLogFields)
-
-	// Viper config management setup
-	cfg = viper.New()
-	err = errors.New("")
+	"github.com/GoogleCloudPlatform/open-match/internal/app/mmlogicapi"
 )
 
 func init() {
-	// Logrus structured logging initialization
-	// Add a hook to the logger to auto-count log lines for metrics output thru OpenCensus
-	log.AddHook(metrics.NewHook(apisrv.MlLogLines, apisrv.KeySeverity))
-
-	// Viper config management initialization
-	cfg, err = config.Read()
-	if err != nil {
-		mlLog.WithFields(log.Fields{
-			"error": err.Error(),
-		}).Error("Unable to load config file")
-	}
-
-	if cfg.GetBool("debug") == true {
-		log.SetLevel(log.DebugLevel) // debug only, verbose - turn off in production!
-		mlLog.Warn("Debug logging configured. Not recommended for production!")
-	}
-
-	// Configure OpenCensus exporter to Prometheus
-	// metrics.ConfigureOpenCensusPrometheusExporter expects that every OpenCensus view you
-	// want to register is in an array, so append any views you want from other
-	// packages to a single array here.
-	ocServerViews := apisrv.DefaultMmlogicAPIViews                      // Matchmaking logic API OpenCensus views.
-	ocServerViews = append(ocServerViews, ocgrpc.DefaultServerViews...) // gRPC OpenCensus views.
-	ocServerViews = append(ocServerViews, config.CfgVarCountView)       // config loader view.
-	// Waiting on https://github.com/opencensus-integrations/redigo/pull/1
-	// ocServerViews = append(ocServerViews, redis.ObservabilityMetricViews...) // redis OpenCensus views.
-	mlLog.WithFields(log.Fields{"viewscount": len(ocServerViews)}).Info("Loaded OpenCensus views")
-	metrics.ConfigureOpenCensusPrometheusExporter(cfg, ocServerViews)
+	mmlogicapi.InitializeApplication()
 }
 
 func main() {
-
-	// Connect to redis
-	pool := redisHelpers.ConnectionPool(cfg)
-	defer pool.Close()
-
-	// Instantiate the gRPC server with the connections we've made
-	mlLog.Info("Attempting to start gRPC server")
-	srv := apisrv.New(cfg, pool)
-
-	// Run the gRPC server
-	err := srv.Open()
-	if err != nil {
-		mlLog.WithFields(log.Fields{"error": err.Error()}).Fatal("Failed to start gRPC server")
-	}
-
-	// Exit when we see a signal
-	terminate := make(chan os.Signal, 1)
-	signal.Notify(terminate, os.Interrupt)
-	<-terminate
-	mlLog.Info("Shutting down gRPC server")
+	mmlogicapi.RunApplication()
 }
