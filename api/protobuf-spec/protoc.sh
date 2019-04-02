6e039cb797faccbd3ed90bf29f097f0b4e83ff26
Jeremy Edwards <1312331+jeremyje@users.noreply.github.com>
2019-04-02 16:42:43 +0000

    Delete images and scripts obsoleted by Makefile.

--- api/protobuf-spec/protoc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. mmlogic.proto
-python3 -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. messages.proto
-cp *pb2* $OM/examples/functions/python3/simple/.
