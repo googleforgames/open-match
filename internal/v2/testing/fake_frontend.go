a0d67c24753c36b84425a244aa30f05b96053312
Jeremy Edwards <1312331+jeremyje@users.noreply.github.com>
2019-04-26 13:53:28 -0700

    Introduce internal/v2/ directory with it's first file, fake_frontend.go.

---
+++ internal/v2/testing/fake_frontend.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2019 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package testing
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/open-match/internal/pb"
+)
+
+// FakeFrontend is an empty gRPC handler.
+type FakeFrontend struct {
+}
+
+// NewFakeFrontend creates a new FakeFrontend for tests.
+func NewFakeFrontend() *FakeFrontend {
+	return &FakeFrontend{}
+}
+
+// CreatePlayer is this service's implementation of the CreatePlayer gRPC method defined in frontend.proto
+func (s *FakeFrontend) CreatePlayer(ctx context.Context, group *pb.CreatePlayerRequest) (*pb.CreatePlayerResponse, error) {
+	return &pb.CreatePlayerResponse{}, nil
+}
+
+// DeletePlayer is this service's implementation of the DeletePlayer gRPC method defined in frontend.proto
+func (s *FakeFrontend) DeletePlayer(ctx context.Context, group *pb.DeletePlayerRequest) (*pb.DeletePlayerResponse, error) {
+	return nil, fmt.Errorf("not supported")
+}
+
+// GetUpdates is this service's implementation of the GetUpdates gRPC method defined in frontend.proto
+func (s *FakeFrontend) GetUpdates(p *pb.GetUpdatesRequest, assignmentStream pb.Frontend_GetUpdatesServer) error {
+	return fmt.Errorf("not supported")
+}
