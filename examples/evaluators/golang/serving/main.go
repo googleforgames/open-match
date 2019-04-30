1277eab4938a955976879f5c61a51225d01eb77f
Jeremy Edwards <1312331+jeremyje@users.noreply.github.com>
2019-04-30 15:22:28 -0700

    Add missing copyright headers and godoc package comments.

--- examples/evaluators/golang/serving/main.go
+++ examples/evaluators/golang/serving/main.go
@@ -1,24 +1,22 @@
-/*
-This is a sample Evaluator built using the Evaluator Harness. It evaluates
-multiple proposals and approves a subset of them. This sample demonstrates
-how to build a basic Evaluator using the Evaluator Harness . This example
-over-simplifies the actual evaluation decisions and hence should not be
-used as is for a real scenario.
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-Copyright 2018 Google LLC
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
+// Package main is a sample Evaluator built using the Evaluator Harness. It evaluates
+// multiple proposals and approves a subset of them. This sample demonstrates
+// how to build a basic Evaluator using the Evaluator Harness . This example
+// over-simplifies the actual evaluation decisions and hence should not be
+// used as is for a real scenario.
 package main
 
 import (
