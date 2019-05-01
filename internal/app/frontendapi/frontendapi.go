6a0c648a8f9c5707d2652d10e06e6edf4ab5201e
Jeremy Edwards <1312331+jeremyje@users.noreply.github.com>
2019-05-01 14:23:35 -0700

    Add missing copyright headers and godoc package comments. (#329)

--- internal/app/frontendapi/frontendapi.go
+++ internal/app/frontendapi/frontendapi.go
@@ -1,26 +1,17 @@
-/*
-This application handles all the startup and connection scaffolding for
-running a gRPC server serving the APIService as defined in
-${OM_ROOT}/internal/pb/frontend.pb.go
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-All the actual important bits are in the API Server source code: apisrv/apisrv.go
-
-Copyright 2018 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Package frontendapi provices the server for frontendapi.
 package frontendapi
 
 import (
