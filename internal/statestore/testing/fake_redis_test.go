afeb05a105b371aa6d7e813531d160cd5955cf94
Jeremy Edwards <1312331+jeremyje@users.noreply.github.com>
2019-05-30 10:52:05 -0700

    Add server/client test helpers and a basic test.

---
+++ internal/statestore/testing/fake_redis_test.go
@@ -0,0 +1,38 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testing
+
+import (
+	"context"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"open-match.dev/open-match/internal/pb"
+	"testing"
+)
+
+func TestFakeStatestore(t *testing.T) {
+	assert := assert.New(t)
+	cfg := viper.New()
+	s, closer := New(t, cfg)
+	defer closer()
+	ctx := context.Background()
+	ticket := &pb.Ticket{
+		Id: "abc",
+	}
+	assert.Nil(s.CreateTicket(ctx, ticket))
+	retrievedTicket, err := s.GetTicket(ctx, "abc")
+	assert.Nil(err)
+	assert.Equal(ticket.Id, retrievedTicket.Id)
+}
