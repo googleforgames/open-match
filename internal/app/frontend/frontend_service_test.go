642f42188777c3472cba72e8fdfa73327b140819
Yufan Fei <yfei816@gmail.com>
2019-06-14 12:04:37 -0700

    Resolve comments

--- internal/app/frontend/frontend_service_test.go
+++ internal/app/frontend/frontend_service_test.go
@@ -86,23 +86,16 @@ func TestDoGetAssignments(t *testing.T) {
 		Id: "test-id",
 	}
 
-	totalAssignments := 2
-	var wantAssignments []*pb.Assignment
-
-	for i := 0; i < totalAssignments; i++ {
-		wantAssignments = append(wantAssignments, &pb.Assignment{Connection: string(i)})
-	}
-
 	tests := []struct {
-		description        string
-		preAction          func(*testing.T, statestore.Service)
-		senderGenerator    func(chan *pb.Assignment, *int) func(*pb.Assignment) error
-		wantCode           codes.Code
-		wantAssignmentChan chan *pb.Assignment
+		description     string
+		preAction       func(*testing.T, statestore.Service, []*pb.Assignment)
+		senderGenerator func(chan *pb.Assignment, *int) func(*pb.Assignment) error
+		wantCode        codes.Code
+		wantAssignments []*pb.Assignment
 	}{
 		{
 			description: "expect error because ticket id does not exist",
-			preAction:   func(_ *testing.T, _ statestore.Service) {},
+			preAction:   func(_ *testing.T, _ statestore.Service, _ []*pb.Assignment) {},
 			senderGenerator: func(tmp chan *pb.Assignment, p *int) func(*pb.Assignment) error {
 				return func(assignment *pb.Assignment) error {
 					*p++
@@ -110,15 +103,15 @@ func TestDoGetAssignments(t *testing.T) {
 					return nil
 				}
 			},
-			wantCode:           codes.NotFound,
-			wantAssignmentChan: make(chan *pb.Assignment, totalAssignments),
+			wantCode:        codes.NotFound,
+			wantAssignments: []*pb.Assignment{},
 		},
 		{
 			description: "expect two assignment reads from preAction writes and fail in grpc aborted code",
-			preAction: func(t *testing.T, store statestore.Service) {
+			preAction: func(t *testing.T, store statestore.Service, wantAssignments []*pb.Assignment) {
 				assert.Nil(t, store.CreateTicket(context.Background(), testTicket))
 				go func() {
-					for i := 0; i < totalAssignments; i++ {
+					for i := 0; i < len(wantAssignments); i++ {
 						time.Sleep(200 * time.Millisecond)
 						assert.Nil(t, store.UpdateAssignments(context.Background(), []string{testTicket.GetId()}, wantAssignments[i]))
 					}
@@ -128,14 +121,14 @@ func TestDoGetAssignments(t *testing.T) {
 				return func(assignment *pb.Assignment) error {
 					*p++
 					tmp <- assignment
-					if len(tmp) == totalAssignments {
+					if len(tmp) == cap(tmp) {
 						return errors.New("some error")
 					}
 					return nil
 				}
 			},
-			wantCode:           codes.Aborted,
-			wantAssignmentChan: make(chan *pb.Assignment, totalAssignments),
+			wantCode:        codes.Aborted,
+			wantAssignments: []*pb.Assignment{&pb.Assignment{Connection: "1"}, &pb.Assignment{Connection: "2"}},
 		},
 	}
 
@@ -145,13 +138,15 @@ func TestDoGetAssignments(t *testing.T) {
 			defer closer()
 
 			senderTriggerCount := 0
-			test.preAction(t, store)
-			err := doGetAssignments(context.Background(), testTicket.GetId(), test.senderGenerator(test.wantAssignmentChan, &senderTriggerCount), store)
+			gotAssignmentChan := make(chan *pb.Assignment, len(test.wantAssignments))
+
+			test.preAction(t, store, test.wantAssignments)
+			err := doGetAssignments(context.Background(), testTicket.GetId(), test.senderGenerator(gotAssignmentChan, &senderTriggerCount), store)
 			assert.Equal(t, test.wantCode, status.Convert(err).Code())
 
 			if err == nil {
-				for i := 0; i < totalAssignments; i++ {
-					assert.Equal(t, <-test.wantAssignmentChan, wantAssignments[i])
+				for i := 0; i < cap(gotAssignmentChan); i++ {
+					assert.Equal(t, <-gotAssignmentChan, test.wantAssignments[i])
 				}
 			}
 		})
