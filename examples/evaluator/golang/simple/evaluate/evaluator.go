be9c9b47d1fdf2eb92d11605ac012ca165baddca
Yufan Fei <yfei816@gmail.com>
2019-06-27 18:22:31 -0700

    Implements a score-based evaluator for end to end test

--- examples/evaluator/golang/simple/evaluate/evaluator.go
+++ examples/evaluator/golang/simple/evaluate/evaluator.go
@@ -14,11 +14,39 @@
 package evaluate
 
 import (
+	"strings"
+
+	"open-match.dev/open-match/examples"
 	harness "open-match.dev/open-match/pkg/harness/evaluator/golang"
 	"open-match.dev/open-match/pkg/pb"
 )
 
 // Evaluate is where your custom evaluation logic lives.
+// This sample evaluator sorts and deduplicates the input matches.
 func Evaluate(p *harness.EvaluatorParams) ([]*pb.Match, error) {
-	return p.Matches, nil
+	scoreBy := func(a, b *pb.Match) bool {
+		return a.GetProperties().GetFields()[examples.MatchScore].GetNumberValue() < b.GetProperties().GetFields()[examples.MatchScore].GetNumberValue()
+	}
+
+	// Sort the input matches based on its score property
+	by(scoreBy).Sort(p.Matches)
+
+	results := []*pb.Match{}
+
+	dedup := map[string]bool{}
+	for _, match := range p.Matches {
+		ids := []string{}
+		for _, ticket := range match.GetTicket() {
+			ids = append(ids, ticket.GetId())
+		}
+
+		// If a match with the same tickets already exists in the map, we ignore the current one since it has lower score.
+		if _, ok := dedup[strings.Join(ids, " ")]; ok {
+			continue
+		}
+
+		results = append(results, match)
+	}
+
+	return results, nil
 }
