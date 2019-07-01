e706c4d766b19d434975c55400d4a9da06433d8d
Scott Redig <sredig@google.com>
2019-07-01 14:02:10 -0700

    progress

--- examples/demo/main.go
+++ examples/demo/main.go
@@ -15,10 +15,48 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"open-match.dev/open-match/internal/config"
+	"open-match.dev/open-match/internal/logging"
+)
+
+var (
+	logger = logrus.WithFields(logrus.Fields{
+		"app":       "openmatch",
+		"component": "endtoend",
+	})
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":51507", nil))
+	cfg, err := config.Read()
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Fatalf("cannot read configuration.")
+	}
+	logging.ConfigureLogging(cfg)
+
+	logger.Info("Initializing Server")
+
+	fileServe := http.FileServer(http.Dir("/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServe))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/dashboard/" {
+			http.NotFound(w, r)
+			return
+		}
+		fileServe.ServeHTTP(w, r)
+	})
+
+	logger.Info("Starting Server")
+
+	// TODO: Other services read their port from the common config map (oddly including
+	// the mmf, which isn't part of the core open-match), how should this be choosing the ports
+	// it exposes?
+	err = http.ListenAndServe(":51507", nil)
+	logger.WithFields(logrus.Fields{
+		"err": err.Error(),
+	}).Fatal("Http ListenAndServe failed.")
 }
