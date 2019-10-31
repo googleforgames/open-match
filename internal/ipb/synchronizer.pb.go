d579de63aa56056a464071cac3b4327d34206670
yfei1 <15369702+yfei1@users.noreply.github.com>
2019-10-31 15:28:34 -0700

    Update proto comments to reflect latest API changes (#932)

--- internal/ipb/synchronizer.pb.go
+++ internal/ipb/synchronizer.pb.go
@@ -250,7 +250,7 @@ type SynchronizerClient interface {
 	// Register associates this request with the current synchronization cycle and
 	// returns an identifier for this registration. The caller returns this
 	// identifier back in the evaluation request. This enables synchronizer to
-	// identify stale evaluation requests belonging to a prior window.
+	// identify stale evaluation requests belonging to a prior cycle.
 	Register(ctx context.Context, in *RegisterRequest, opts ...grpc.CallOption) (*RegisterResponse, error)
 	// EvaluateProposals accepts a list of proposals and a registration identifier
 	// for this request. If the synchronization cycle to which the request was
@@ -314,7 +314,7 @@ type SynchronizerServer interface {
 	// Register associates this request with the current synchronization cycle and
 	// returns an identifier for this registration. The caller returns this
 	// identifier back in the evaluation request. This enables synchronizer to
-	// identify stale evaluation requests belonging to a prior window.
+	// identify stale evaluation requests belonging to a prior cycle.
 	Register(context.Context, *RegisterRequest) (*RegisterResponse, error)
 	// EvaluateProposals accepts a list of proposals and a registration identifier
 	// for this request. If the synchronization cycle to which the request was
