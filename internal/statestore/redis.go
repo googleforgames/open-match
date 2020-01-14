eaaafe88585967c4c27d848cabf5388fc20d31d1
Yufan Fei <yfei816@gmail.com>
2020-01-13 17:50:40 -0800

    Enable and establish Redis connections via Sentinel

--- internal/statestore/redis.go
+++ internal/statestore/redis.go
@@ -55,61 +55,126 @@ func (rb *redisBackend) Close() error {
 
 // newRedis creates a statestore.Service backed by Redis database.
 func newRedis(cfg config.View) Service {
-	// As per https://www.iana.org/assignments/uri-schemes/prov/redis
-	// redis://user:secret@localhost:6379/0?foo=bar&qux=baz
-
-	// Add redis user and password to connection url if they exist
-	redisURL := "redis://"
-	maskedURL := redisURL
-
-	passwordFile := cfg.GetString("redis.passwordPath")
-	if len(passwordFile) > 0 {
-		redisLogger.Debugf("loading Redis password from file %s", passwordFile)
-		passwordData, err := ioutil.ReadFile(passwordFile)
-		if err != nil {
-			redisLogger.Fatalf("cannot read Redis password from file %s, desc: %s", passwordFile, err.Error())
-		}
-		redisURL += fmt.Sprintf("%s:%s@", cfg.GetString("redis.user"), string(passwordData))
-		maskedURL += fmt.Sprintf("%s:%s@", cfg.GetString("redis.user"), "**********")
-	}
-	redisURL += cfg.GetString("redis.hostname") + ":" + cfg.GetString("redis.port")
-	maskedURL += cfg.GetString("redis.hostname") + ":" + cfg.GetString("redis.port")
-
-	redisLogger.WithField("redisURL", maskedURL).Debug("Attempting to connect to Redis")
-
-	pool := &redis.Pool{
-		MaxIdle:     cfg.GetInt("redis.pool.maxIdle"),
-		MaxActive:   cfg.GetInt("redis.pool.maxActive"),
-		IdleTimeout: cfg.GetDuration("redis.pool.idleTimeout"),
-		Wait:        true,
-		TestOnBorrow: func(c redis.Conn, _ time.Time) error {
-			_, err := c.Do("PING")
-			return err
-		},
-		DialContext: func(ctx context.Context) (redis.Conn, error) {
-			if ctx.Err() != nil {
-				return nil, ctx.Err()
-			}
-			return redis.DialURL(redisURL, redis.DialConnectTimeout(cfg.GetDuration("redis.pool.idleTimeout")), redis.DialReadTimeout(cfg.GetDuration("redis.pool.idleTimeout")))
-		},
-	}
-	healthCheckPool := &redis.Pool{
-		MaxIdle:     3,
-		MaxActive:   0,
-		IdleTimeout: 10 * cfg.GetDuration("redis.pool.healthCheckTimeout"),
-		Wait:        true,
-		DialContext: func(ctx context.Context) (redis.Conn, error) {
-			if ctx.Err() != nil {
-				return nil, ctx.Err()
-			}
-			return redis.DialURL(redisURL, redis.DialConnectTimeout(cfg.GetDuration("redis.pool.healthCheckTimeout")), redis.DialReadTimeout(cfg.GetDuration("redis.pool.healthCheckTimeout")))
-		},
-	}
-
 	return &redisBackend{
-		healthCheckPool: healthCheckPool,
-		redisPool:       pool,
+		healthCheckPool: getRedisPool("healthcheck", cfg),
+		redisPool:       getRedisPool("master", cfg),
 		cfg:             cfg,
+	}
+}
+
+func getRedisPool(poolType string, cfg config.View) *redis.Pool {
+	var maxIdle, maxActive int
+	var idleTimeout time.Duration
+	var sentinelPool *redis.Pool
+
+	if cfg.GetBool("redis.sentinelEnabled") {
+		sentinelAddr := fmt.Sprintf("%s:%s", cfg.GetString("redis.sentinelHostname"), cfg.GetString("redis.sentinelPort"))
+		sentinelURL := redisURLFromAddr(sentinelAddr, cfg)
+		sentinelPool = &redis.Pool{
+			MaxIdle:      maxIdle,
+			MaxActive:    maxActive,
+			IdleTimeout:  idleTimeout,
+			Wait:         true,
+			TestOnBorrow: testOnBorrow,
+			DialContext: func(ctx context.Context) (redis.Conn, error) {
+				if ctx.Err() != nil {
+					return nil, ctx.Err()
+				}
+				redisLogger.WithField("sentinelAddr", sentinelAddr).Debug("Attempting to connect to Redis Sentinel")
+				return redis.DialURL(sentinelURL, redis.DialConnectTimeout(idleTimeout), redis.DialReadTimeout(idleTimeout))
+			},
+		}
+	}
+
+	switch poolType {
+	case "healthcheck":
+		maxIdle = 3
+		maxActive = 0
+		idleTimeout = 10 * cfg.GetDuration("redis.pool.healthCheckTimeout")
+	default:
+		maxIdle = cfg.GetInt("redis.pool.maxIdle")
+		maxActive = cfg.GetInt("redis.pool.maxActive")
+		idleTimeout = cfg.GetDuration("redis.pool.idleTimeout")
+	}
+
+	return &redis.Pool{
+		MaxIdle:      maxIdle,
+		MaxActive:    maxActive,
+		IdleTimeout:  idleTimeout,
+		Wait:         true,
+		TestOnBorrow: testOnBorrow,
+		DialContext:  getDialFunc(poolType, cfg, sentinelPool),
+	}
+}
+
+func getDialFunc(poolType string, cfg config.View, sentinelPool *redis.Pool) func(ctx context.Context) (redis.Conn, error) {
+	idleTimeout := cfg.GetDuration("redis.pool.idleTimeout")
+	healthCheckTimeout := cfg.GetDuration("redis.pool.healthCheckTimeout")
+
+	if cfg.GetBool("redis.sentinelEnabled") {
+		sentinelAddr := fmt.Sprintf("%s:%s", cfg.GetString("redis.sentinelHostname"), cfg.GetString("redis.sentinelPort"))
+		sentinelURL := redisURLFromAddr(sentinelAddr, cfg)
+
+		switch poolType {
+		case "healthcheck":
+			return func(ctx context.Context) (redis.Conn, error) {
+				if ctx.Err() != nil {
+					return nil, ctx.Err()
+				}
+				return redis.DialURL(sentinelURL, redis.DialConnectTimeout(healthCheckTimeout), redis.DialReadTimeout(healthCheckTimeout))
+			}
+		case "master":
+			return func(ctx context.Context) (redis.Conn, error) {
+				if ctx.Err() != nil {
+					return nil, ctx.Err()
+				}
+
+				sentinelConn, err := sentinelPool.GetContext(ctx)
+				if err != nil {
+					redisLogger.WithFields(logrus.Fields{
+						"error": err.Error(),
+					}).Error("failed to connect to redis sentinel")
+					return nil, status.Errorf(codes.Unavailable, "%v", err)
+				}
+
+				masterInfo, err := redis.Strings(sentinelConn.Do("SENTINEL", "GET-MASTER-ADDR-BY-NAME", cfg.GetString("redis.sentinelMaster")))
+				if err != nil {
+					redisLogger.WithFields(logrus.Fields{
+						"error": err.Error(),
+					}).Error("failed to get current master from redis sentinel")
+					return nil, status.Errorf(codes.Unavailable, "%v", err)
+				}
+
+				masterURL := redisURLFromAddr(fmt.Sprintf("%s:%s", masterInfo[0], masterInfo[1]), cfg)
+				return redis.DialURL(masterURL, redis.DialConnectTimeout(idleTimeout), redis.DialReadTimeout(idleTimeout))
+			}
+		default:
+			redisLogger.Fatalf("dial type: %s not supported, sentinelEnabled: %v", poolType, cfg.GetBool("redis.sentinelEnabled"))
+			return nil
+		}
+	} else {
+		masterAddr := fmt.Sprintf("%s:%s", cfg.GetString("redis.hostname"), cfg.GetString("redis.port"))
+		masterURL := redisURLFromAddr(masterAddr, cfg)
+
+		switch poolType {
+		case "healthcheck":
+			return func(ctx context.Context) (redis.Conn, error) {
+				if ctx.Err() != nil {
+					return nil, ctx.Err()
+				}
+				return redis.DialURL(masterURL, redis.DialConnectTimeout(healthCheckTimeout), redis.DialReadTimeout(healthCheckTimeout))
+			}
+		case "master":
+			return func(ctx context.Context) (redis.Conn, error) {
+				if ctx.Err() != nil {
+					return nil, ctx.Err()
+				}
+				return redis.DialURL(masterURL, redis.DialConnectTimeout(idleTimeout), redis.DialReadTimeout(idleTimeout))
+			}
+		default:
+			redisLogger.Fatalf("dial type: %s not supported, sentinelEnabled: %v", poolType, cfg.GetBool("redis.sentinelEnabled"))
+			return nil
+		}
 	}
 }
 
@@ -132,6 +197,35 @@ func (rb *redisBackend) HealthCheck(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+func testOnBorrow(c redis.Conn, t time.Time) error {
+	// Assume the connection is valid if it was used in 30 sec.
+	if time.Since(t) < 30*time.Second {
+		return nil
+	}
+	_, err := c.Do("PING")
+	return err
+}
+
+func redisURLFromAddr(addr string, cfg config.View) string {
+	// As per https://www.iana.org/assignments/uri-schemes/prov/redis
+	// redis://user:secret@localhost:6379/0?foo=bar&qux=baz
+
+	// Add redis user and password to connection url if they exist
+	redisURL := "redis://"
+
+	passwordFile := cfg.GetString("redis.passwordPath")
+	if len(passwordFile) > 0 {
+		redisLogger.Debugf("loading Redis password from file %s", passwordFile)
+		passwordData, err := ioutil.ReadFile(passwordFile)
+		if err != nil {
+			redisLogger.Fatalf("cannot read Redis password from file %s, desc: %s", passwordFile, err.Error())
+		}
+		redisURL += fmt.Sprintf("%s:%s@", cfg.GetString("redis.user"), string(passwordData))
+	}
+
+	return redisURL + addr
 }
 
 func (rb *redisBackend) connect(ctx context.Context) (redis.Conn, error) {
--- internal/statestore/redis_test.go
+++ internal/statestore/redis_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/Bose/minisentinel"
 	miniredis "github.com/alicebob/miniredis/v2"
 	"github.com/rs/xid"
 	"github.com/spf13/viper"
@@ -36,7 +37,7 @@ import (
 
 func TestStatestoreSetup(t *testing.T) {
 	assert := assert.New(t)
-	cfg, closer := createRedis(t)
+	cfg, closer := createRedis(t, true)
 	defer closer()
 	service := New(cfg)
 	assert.NotNil(service)
@@ -46,7 +47,7 @@ func TestStatestoreSetup(t *testing.T) {
 func TestTicketLifecycle(t *testing.T) {
 	// Create State Store
 	assert := assert.New(t)
-	cfg, closer := createRedis(t)
+	cfg, closer := createRedis(t, true)
 	defer closer()
 	service := New(cfg)
 	assert.NotNil(service)
@@ -104,7 +105,7 @@ func TestTicketLifecycle(t *testing.T) {
 func TestIgnoreLists(t *testing.T) {
 	// Create State Store
 	assert := assert.New(t)
-	cfg, closer := createRedis(t)
+	cfg, closer := createRedis(t, true)
 	defer closer()
 	service := New(cfg)
 	assert.NotNil(service)
@@ -153,7 +154,7 @@ func TestIgnoreLists(t *testing.T) {
 func TestTicketIndexing(t *testing.T) {
 	// Create State Store
 	assert := assert.New(t)
-	cfg, closer := createRedis(t)
+	cfg, closer := createRedis(t, true)
 	defer closer()
 	service := New(cfg)
 	assert.NotNil(service)
@@ -229,7 +230,7 @@ func TestTicketIndexing(t *testing.T) {
 func TestGetAssignmentBeforeSet(t *testing.T) {
 	// Create State Store
 	assert := assert.New(t)
-	cfg, closer := createRedis(t)
+	cfg, closer := createRedis(t, true)
 	defer closer()
 	service := New(cfg)
 	assert.NotNil(service)
@@ -250,7 +251,7 @@ func TestGetAssignmentBeforeSet(t *testing.T) {
 func TestUpdateAssignmentFatal(t *testing.T) {
 	// Create State Store
 	assert := assert.New(t)
-	cfg, closer := createRedis(t)
+	cfg, closer := createRedis(t, true)
 	defer closer()
 	service := New(cfg)
 	assert.NotNil(service)
@@ -276,7 +277,7 @@ func TestUpdateAssignmentFatal(t *testing.T) {
 func TestGetAssignmentNormal(t *testing.T) {
 	// Create State Store
 	assert := assert.New(t)
-	cfg, closer := createRedis(t)
+	cfg, closer := createRedis(t, true)
 	defer closer()
 	service := New(cfg)
 	assert.NotNil(service)
@@ -317,7 +318,7 @@ func TestGetAssignmentNormal(t *testing.T) {
 func TestUpdateAssignmentNormal(t *testing.T) {
 	// Create State Store
 	assert := assert.New(t)
-	cfg, closer := createRedis(t)
+	cfg, closer := createRedis(t, true)
 	defer closer()
 	service := New(cfg)
 	assert.NotNil(service)
@@ -349,9 +350,9 @@ func TestUpdateAssignmentNormal(t *testing.T) {
 	assert.Nil(err)
 }
 
-func TestConnect(t *testing.T) {
+func TestConnectWithSentinel(t *testing.T) {
 	assert := assert.New(t)
-	cfg, closer := createRedis(t)
+	cfg, closer := createRedis(t, true)
 	defer closer()
 	store := New(cfg)
 	defer store.Close()
@@ -369,19 +370,38 @@ func TestConnect(t *testing.T) {
 	assert.Nil(conn)
 }
 
-func createRedis(t *testing.T) (config.View, func()) {
+func TestConnectWithoutSentinel(t *testing.T) {
+	assert := assert.New(t)
+	cfg, closer := createRedis(t, false)
+	defer closer()
+	store := New(cfg)
+	defer store.Close()
+	ctx := utilTesting.NewContext(t)
+
+	is, ok := store.(*instrumentedService)
+	assert.True(ok)
+	rb, ok := is.s.(*redisBackend)
+	assert.True(ok)
+
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+	conn, err := rb.connect(ctx)
+	assert.NotNil(err)
+	assert.Nil(conn)
+}
+
+func createRedis(t *testing.T, withSentinel bool) (config.View, func()) {
 	cfg := viper.New()
-	mredis, err := miniredis.Run()
+	mredis := miniredis.NewMiniRedis()
+	err := mredis.StartAddr("localhost:0")
 	if err != nil {
-		t.Fatalf("cannot create redis %s", err)
+		t.Fatalf("failed to start miniredis, %v", err)
 	}
 
-	cfg.Set("redis.hostname", mredis.Host())
-	cfg.Set("redis.port", mredis.Port())
-	cfg.Set("redis.pool.maxIdle", 1000)
+	cfg.Set("redis.pool.maxIdle", 5)
 	cfg.Set("redis.pool.idleTimeout", time.Second)
 	cfg.Set("redis.pool.healthCheckTimeout", 100*time.Millisecond)
-	cfg.Set("redis.pool.maxActive", 1000)
+	cfg.Set("redis.pool.maxActive", 5)
 	cfg.Set("redis.expiration", 42000)
 	cfg.Set("storage.ignoreListTTL", "200ms")
 	cfg.Set("backoff.initialInterval", 100*time.Millisecond)
@@ -391,7 +411,32 @@ func createRedis(t *testing.T) (config.View, func()) {
 	cfg.Set("backoff.maxElapsedTime", 100*time.Millisecond)
 	cfg.Set(telemetry.ConfigNameEnableMetrics, true)
 
-	return cfg, func() { mredis.Close() }
+	var closer func()
+	if withSentinel {
+		s := minisentinel.NewSentinel(mredis)
+		err = s.StartAddr("localhost:0")
+		if err != nil {
+			t.Fatalf("failed to start minisentinel, %v", err)
+		}
+
+		cfg.Set("redis.sentinelHostname", s.Host())
+		cfg.Set("redis.sentinelPort", s.Port())
+		cfg.Set("redis.sentinelMaster", s.MasterInfo().Name)
+		cfg.Set("redis.sentinelEnabled", true)
+
+		closer = func() {
+			s.Close()
+			mredis.Close()
+		}
+	} else {
+		cfg.Set("redis.hostname", mredis.Host())
+		cfg.Set("redis.port", mredis.Port())
+		closer = func() {
+			mredis.Close()
+		}
+	}
+
+	return cfg, closer
 }
 
 // TODO: test Redis connection with Auth
