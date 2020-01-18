1a0ac26899ce071eefcefc6b4c669f14b748f84a
Caleb Atwood <calebatwd@gmail.com>
2020-01-17 17:59:49 -0800

    Grpc and swagger generated files

--- internal/app/backend/backend_service_test.go
+++ internal/app/backend/backend_service_test.go
@@ -168,6 +168,129 @@ func TestDoFetchMatchesFilterChannel(t *testing.T) {
 	}
 }
 
+func TestdoReleaseTickets(t *testing.T) {
+	fakeProperty := "test-property"
+	fakeTickets := []*pb.Ticket{
+		{
+			Id: "1",
+			SearchFields: &pb.SearchFields{
+				DoubleArgs: map[string]float64{
+					fakeProperty: 1,
+				},
+			},
+		},
+		{
+			Id: "2",
+			SearchFields: &pb.SearchFields{
+				DoubleArgs: map[string]float64{
+					fakeProperty: 2,
+				},
+			},
+		},
+	}
+
+	tests := []struct {
+		description string
+		preAction   func(context.Context, context.CancelFunc, statestore.Service)
+		req         *pb.ReleaseTicketsRequest
+		wantCode    codes.Code
+	}{
+		{
+			description: "expect unavailable code since context is canceled before being called",
+			preAction: func(_ context.Context, cancel context.CancelFunc, _ statestore.Service) {
+				cancel()
+			},
+			req: &pb.ReleaseTicketsRequest{
+				TicketIds: []string{"1"},
+			},
+			wantCode: codes.Unavailable,
+		},
+		{
+			description: "expect invalid argument code since assignment is nil",
+			preAction: func(_ context.Context, cancel context.CancelFunc, _ statestore.Service) {
+				cancel()
+			},
+			req:      &pb.AssignTicketsRequest{},
+			wantCode: codes.InvalidArgument,
+		},
+		{
+			description: "expect not found code since ticket does not exist",
+			preAction:   func(_ context.Context, _ context.CancelFunc, _ statestore.Service) {},
+			req: &pb.AssignTicketsRequest{
+				TicketIds: []string{"1", "2"},
+				Assignment: &pb.Assignment{
+					Connection: "123",
+				},
+			},
+			wantCode: codes.NotFound,
+		},
+		{
+			description: "expect ok code",
+			preAction: func(ctx context.Context, cancel context.CancelFunc, store statestore.Service) {
+				for _, fakeTicket := range fakeTickets {
+					store.CreateTicket(ctx, fakeTicket)
+					store.IndexTicket(ctx, fakeTicket)
+				}
+				// Make sure tickets are correctly indexed.
+				var wantFilteredTickets []*pb.Ticket
+				pool := &pb.Pool{
+					DoubleRangeFilters: []*pb.DoubleRangeFilter{{DoubleArg: fakeProperty, Min: 0, Max: 3}},
+				}
+				err := store.FilterTickets(ctx, pool, 10, func(filterTickets []*pb.Ticket) error {
+					wantFilteredTickets = filterTickets
+					return nil
+				})
+				assert.Nil(t, err)
+				assert.Equal(t, len(fakeTickets), len(wantFilteredTickets))
+			},
+			req: &pb.AssignTicketsRequest{
+				TicketIds: []string{"1", "2"},
+				Assignment: &pb.Assignment{
+					Connection: "123",
+				},
+			},
+			wantCode: codes.OK,
+			wantAssignment: &pb.Assignment{
+				Connection: "123",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(utilTesting.NewContext(t))
+			cfg := viper.New()
+			store, closer := statestoreTesting.NewStoreServiceForTesting(t, cfg)
+			defer closer()
+
+			test.preAction(ctx, cancel, store)
+
+			err := doAssignTickets(ctx, test.req, store)
+
+			assert.Equal(t, test.wantCode, status.Convert(err).Code())
+
+			if err == nil {
+				for _, id := range test.req.GetTicketIds() {
+					ticket, err := store.GetTicket(ctx, id)
+					assert.Nil(t, err)
+					assert.Equal(t, test.wantAssignment, ticket.GetAssignment())
+				}
+
+				// Make sure tickets are deindexed after assignment
+				var wantFilteredTickets []*pb.Ticket
+				pool := &pb.Pool{
+					DoubleRangeFilters: []*pb.DoubleRangeFilter{{DoubleArg: fakeProperty, Min: 0, Max: 2}},
+				}
+				store.FilterTickets(ctx, pool, 10, func(filterTickets []*pb.Ticket) error {
+					wantFilteredTickets = filterTickets
+					return nil
+				})
+				assert.Nil(t, wantFilteredTickets)
+			}
+		})
+	}
+}
+
 func TestDoAssignTickets(t *testing.T) {
 	fakeProperty := "test-property"
 	fakeTickets := []*pb.Ticket{
--- pkg/pb/backend.pb.gw.go
+++ pkg/pb/backend.pb.gw.go
@@ -13,7 +13,6 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/golang/protobuf/descriptor"
 	"github.com/golang/protobuf/proto"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/grpc-ecosystem/grpc-gateway/utilities"
@@ -23,13 +22,11 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-// Suppress "imported and not used" errors
 var _ codes.Code
 var _ io.Reader
 var _ status.Status
 var _ = runtime.String
 var _ = utilities.NewDoubleArray
-var _ = descriptor.ForMessage
 
 func request_Backend_FetchMatches_0(ctx context.Context, marshaler runtime.Marshaler, client BackendClient, req *http.Request, pathParams map[string]string) (Backend_FetchMatchesClient, runtime.ServerMetadata, error) {
 	var protoReq FetchMatchesRequest
@@ -73,8 +70,8 @@ func request_Backend_AssignTickets_0(ctx context.Context, marshaler runtime.Mars
 
 }
 
-func local_request_Backend_AssignTickets_0(ctx context.Context, marshaler runtime.Marshaler, server BackendServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq AssignTicketsRequest
+func request_Backend_ReleaseTickets_0(ctx context.Context, marshaler runtime.Marshaler, client BackendClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ReleaseTicketsRequest
 	var metadata runtime.ServerMetadata
 
 	newReader, berr := utilities.IOReaderFactory(req.Body)
@@ -85,44 +82,9 @@ func local_request_Backend_AssignTickets_0(ctx context.Context, marshaler runtim
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.AssignTickets(ctx, &protoReq)
+	msg, err := client.ReleaseTickets(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
-}
-
-// RegisterBackendHandlerServer registers the http handlers for service Backend to "mux".
-// UnaryRPC     :call BackendServer directly.
-// StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
-func RegisterBackendHandlerServer(ctx context.Context, mux *runtime.ServeMux, server BackendServer) error {
-
-	mux.Handle("POST", pattern_Backend_FetchMatches_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		err := status.Error(codes.Unimplemented, "streaming calls are not yet supported in the in-process transport")
-		_, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-		return
-	})
-
-	mux.Handle("POST", pattern_Backend_AssignTickets_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_Backend_AssignTickets_0(rctx, inboundMarshaler, server, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_Backend_AssignTickets_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	return nil
 }
 
 // RegisterBackendHandlerFromEndpoint is same as RegisterBackendHandler but
@@ -203,6 +165,26 @@ func RegisterBackendHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 
 	})
 
+	mux.Handle("POST", pattern_Backend_ReleaseTickets_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Backend_ReleaseTickets_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Backend_ReleaseTickets_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -210,10 +192,14 @@ var (
 	pattern_Backend_FetchMatches_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "backend", "matches"}, "fetch", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Backend_AssignTickets_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "backend", "tickets"}, "assign", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_Backend_ReleaseTickets_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "backend", "tickets"}, "release", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
 	forward_Backend_FetchMatches_0 = runtime.ForwardResponseStream
 
 	forward_Backend_AssignTickets_0 = runtime.ForwardResponseMessage
+
+	forward_Backend_ReleaseTickets_0 = runtime.ForwardResponseMessage
 )
