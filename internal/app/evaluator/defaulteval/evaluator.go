1b5a667e002a05eef805a11fa17e404f378ae097
Scott Redig <sredig@google.com>
2020-02-14 14:27:58 -0800

    fix lint

--- internal/app/evaluator/defaulteval/evaluator.go
+++ internal/app/evaluator/defaulteval/evaluator.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Default evaluator implementation.
+// Package defaulteval provides a simple score based evaluator.
 package defaulteval
 
 import (
