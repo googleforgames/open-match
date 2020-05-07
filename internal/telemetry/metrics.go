f6d0e9bd112224f907e9df6b72b337f2b7cd0911
Yufan Fei <yfei816@gmail.com>
2020-05-06 18:54:57 -0700

    update

--- internal/telemetry/metrics.go
+++ internal/telemetry/metrics.go
@@ -15,8 +15,11 @@
 package telemetry
 
 import (
+	"context"
+
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
 )
 
 // Default histogram distributions
@@ -26,54 +29,93 @@ var (
 	DefaultCountDistribution        = view.Distribution(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536)
 )
 
-// Ticket metrics
-var (
-	TotalBytesPerTicket   = stats.Int64("openmatch.dev/total_bytes_per_ticket", "Total bytes per ticket", stats.UnitBytes)
-	SearchFieldsPerTicket = stats.Int64("openmatch.dev/searchfields_per_ticket", "Searchfields per ticket", stats.UnitDimensionless)
-	TicketsAssigned       = stats.Int64("openmatch.dev/tickets_assigned", "Number of tickets assigned per request", stats.UnitDimensionless)
-	TicketsReleased       = stats.Int64("openmatch.dev/tickets_released", "Number of tickets released per request", stats.UnitDimensionless)
-)
+// Gauge creates a gauge metric to be recorded with dimensionless unit.
+func Gauge(name string, description string, tags ...tag.Key) *stats.Int64Measure {
+	s := stats.Int64(name, description, "1")
+	gaugeView(s, tags...)
+	return s
+}
 
-// Match metrics
-var (
-	TotalBytesPerMatch = stats.Int64("openmatch.dev/total_bytes_per_match", "Total bytes per match", stats.UnitBytes)
-	TicketsPerMatch    = stats.Int64("openmatch.dev/tickets_per_match", "Number of tickets per match", stats.UnitDimensionless)
-)
+// SetGauge sets the value of the metric
+func SetGauge(ctx context.Context, s *stats.Int64Measure, n int64, tags ...tag.Mutator) {
+	mCtx, err := tag.New(ctx, tags...)
+	if err != nil {
+		return
+	}
+	stats.Record(mCtx, s.M(n))
+}
 
-// Query metrics
-var (
-	TicketsPerQuery          = stats.Int64("openmatch.dev/query/tickets_per_query", "Number of tickets per query", stats.UnitDimensionless)
-	QueryCacheTotalItems     = stats.Int64("openmatch.dev/query/total_cache_items", "Total number of tickets query service cached", stats.UnitDimensionless)
-	QueryCacheDeltaItems     = stats.Int64("openmatch.dev/query/delta_cache_items", "Number of tickets added by query service in the last update", stats.UnitDimensionless)
-	QueryCacheWaitingQueries = stats.Int64("openmatch.dev/query/waiting_queries", "Number of waiting queries in the last update", stats.UnitDimensionless)
-	QueryCacheUpdateLatency  = stats.Float64("openmatch.dev/query/update_latency", "Time elapsed of each query cache update", stats.UnitMilliseconds)
-)
+// Counter creates a counter metric to be recorded with dimensionless unit.
+func Counter(name string, description string, tags ...tag.Key) *stats.Int64Measure {
+	s := stats.Int64(name, "Count of "+description+".", stats.UnitDimensionless)
+	counterView(s, tags...)
+	return s
+}
 
-// Evaluator metrics
-var (
-	MatchesPerEvaluateRequest  = stats.Int64("openmatch.dev/evaluator/matches_per_request", "Number of matches sent to the evaluator per request", stats.UnitDimensionless)
-	MatchesPerEvaluateResponse = stats.Int64("openmatch.dev/evaluator/matches_per_response", "Number of matches returned by the evaluator per response", stats.UnitDimensionless)
-	CollidedMatchesPerEvaluate = stats.Int64("openmatch.dev/evaluator/collided_matches_per_call", "Number of collided matches per default evaluator call", stats.UnitDimensionless)
-)
+// HistogramWithBounds creates a prometheus histogram metric to be recorded with specified bounds and metric type.
+func HistogramWithBounds(name string, description string, unit string, bounds []float64, tags ...tag.Key) *stats.Int64Measure {
+	s := stats.Int64(name, description, unit)
+	histogramView(s, bounds, tags...)
+	return s
+}
 
-// Synchronizer metrics
-var (
-	SynchronizerIterationLatency        = stats.Float64("openmatch.dev/synchronizer/iteration_latency", "Time elapsed of each synchronizer iteration", stats.UnitMilliseconds)
-	SynchronizerRegistrationWaitTime    = stats.Float64("openmatch.dev/synchronizer/registration_wait_time", "Time elapsed of registration wait time", stats.UnitMilliseconds)
-	SynchronizerRegistrationMMFDoneTime = stats.Float64("openmatch.dev/synchronizer/registration_mmf_done_time", "Time elapsed wasted in registration window with done MMFs", stats.UnitMilliseconds)
-)
+// RecordUnitMeasurement records a data point using the input metric by one unit with given tags.
+func RecordUnitMeasurement(ctx context.Context, s *stats.Int64Measure, tags ...tag.Mutator) {
+	RecordNUnitMeasurement(ctx, s, 1, tags...)
+}
 
-// Health metrics
-var (
-	ProbeReadiness = stats.Int64("openmatch.dev/health/readiness", "Count of readiness probes", stats.UnitDimensionless)
-)
+// RecordNUnitMeasurement records a data point using the input metric by N units with given tags.
+func RecordNUnitMeasurement(ctx context.Context, s *stats.Int64Measure, n int64, tags ...tag.Mutator) {
+	if err := stats.RecordWithTags(ctx, tags, s.M(n)); err != nil {
+		logger.WithError(err).Infof("cannot record stat with tags %#v", tags)
+		return
+	}
+}
 
-func measureToDefaultView(s stats.Measure, metricName, metricDesc string, aggregation *view.Aggregation) *view.View {
+// histogramView converts the measurement into a view for a histogram metric.
+func histogramView(s *stats.Int64Measure, bounds []float64, tags ...tag.Key) *view.View {
 	v := &view.View{
-		Name:        metricName,
-		Description: metricDesc,
+		Name:        s.Name(),
 		Measure:     s,
-		Aggregation: aggregation,
+		Description: s.Description(),
+		Aggregation: view.Distribution(bounds...),
+		TagKeys:     tags,
+	}
+	err := view.Register(v)
+	if err != nil {
+		logger.WithError(err).Infof("cannot register view for metric: %s, it will not be reported", s.Name())
+	}
+	return v
+}
+
+// gaugeView converts the measurement into a view for a gauge metric.
+func gaugeView(s *stats.Int64Measure, tags ...tag.Key) *view.View {
+	v := &view.View{
+		Name:        s.Name(),
+		Measure:     s,
+		Description: s.Description(),
+		Aggregation: view.LastValue(),
+		TagKeys:     tags,
+	}
+	err := view.Register(v)
+	if err != nil {
+		logger.WithError(err).Infof("cannot register view for metric: %s, it will not be reported", s.Name())
+	}
+	return v
+}
+
+// counterView converts the measurement into a view for a counter.
+func counterView(s *stats.Int64Measure, tags ...tag.Key) *view.View {
+	v := &view.View{
+		Name:        s.Name(),
+		Measure:     s,
+		Description: s.Description(),
+		Aggregation: view.Count(),
+		TagKeys:     tags,
+	}
+	err := view.Register(v)
+	if err != nil {
+		logger.WithError(err).Infof("cannot register view for metric: %s, it will not be reported", s.Name())
 	}
 	return v
 }
